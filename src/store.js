bcf0babea96e006f68dc1b28a6b773c48ee7043b
Therakar <matteo.pau200@gmail.com>
2024-03-05 12:23:27 +0100

    Adds action creator functions

--- src/store.js
+++ src/store.js
@@ -1,3 +1,93 @@
 /* Here is where I'm going to write the Redux pure code 
 (so basically, Redux without React), 
 so that I can understand Redux in isolation first. */
+
+import { createStore } from "redux";
+
+// 1. Create an initialState Object
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+// 2. Define the reducer() function
+function reducer(state = initialState, action) {
+  // 3. Define the action.type(s) and define what are they gonna return
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+// Without action creators functions
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/withdraw", payload: 500 });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "buy a car" },
+// });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+// ACTION CREATORS FUNCTIONS
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount: amount, purpose: purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+console.log(store.getState());
+
+store.dispatch(withdraw(200));
+console.log(store.getState());
+
+store.dispatch(requestLoan(1000, "Buy a car"));
+console.log(store.getState());
+
+store.dispatch(payLoan());
+console.log(store.getState());
